5c123455a5dfae1b899683c7181dd6a1967a52a0
Ryan McGuire <ryan@enigmacurry.com>
2019-04-15 12:08:39 -0400

    start.sh uses external config

--- lbrycrd/start.sh
+++ lbrycrd/start.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-## Set config params
-## TODO: Make this more automagic in the future.
-echo "rpcuser=$RPC_USER" > /data/.lbrycrd/lbrycrd.conf
-echo "rpcpassword=$RPC_PASSWORD" >> /data/.lbrycrd/lbrycrd.conf
-echo "rpcallowip=$RPC_ALLOW_IP" >> /data/.lbrycrd/lbrycrd.conf
-echo "rpcport=9245" >> /data/.lbrycrd/lbrycrd.conf
-echo "rpcbind=0.0.0.0" >> /data/.lbrycrd/lbrycrd.conf
-#echo "bind=0.0.0.0" >> /data/.lbrycrd/lbrycrd.conf
-
 ## Control this invocation through envvar.
 case $RUN_MODE in
   default )
